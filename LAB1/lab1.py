1eae61c38b5abd7f6a92d963f13b4a3084ace640
urvishd <push.default>
2021-08-06 22:04:38 +0530

    1st part complete

---
+++ LAB1/lab1.py
@@ -0,0 +1,45 @@
+import nltk # Python library for NLP
+from nltk.corpus import twitter_samples # sample Twitter dataset from NLTK
+import matplotlib.pyplot as plt # library for visualization
+import random
+
+nltk.download('twitter_samples')
+
+all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+
+print('Number of positive tweets: ', len(all_positive_tweets))
+print('Number of negative tweets: ', len(all_negative_tweets))
+print('\nThe type of all_positive_tweets is: ', type(all_positive_tweets))
+print('The type of a tweet entry is: ', type(all_negative_tweets[0]))
+
+fig = plt.figure(figsize=(10, 5))
+labels = 'ML-BSB-Lec', 'ML-HAP-Lec','ML-HAP-Lab'
+sizes = [40, 35, 25]
+
+plt.pie(sizes, labels=labels, autopct='%.2f%%',
+shadow=True, startangle=90)
+# Equal aspect ratio ensures that pie is drawn as a circle.
+plt.axis('equal')
+
+plt.show()
+
+
+
+fig = plt.figure(figsize=(5, 5))
+# labels for the two classes
+labels = 'Positives', 'Negative'
+# Sizes for each slide
+sizes = [len(all_positive_tweets), len(all_negative_tweets)]
+# Declare pie chart, where the slices will be ordered and plotted counter-clockwise:
+plt.pie(sizes, labels=labels, autopct='%1.1f%%',
+shadow=True, startangle=90)
+# Equal aspect ratio ensures that pie is drawn as a circle.
+plt.axis('equal')
+# Display the chart
+plt.show()
+
+
+print('\033[92m' + all_positive_tweets[random.randint(0,5000)])
+# print negative in red
+print('\033[91m' + all_negative_tweets[random.randint(0,5000)])
